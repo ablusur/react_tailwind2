0f3c075287b62aae900fd52a2f3edc86f94b466c
Ablusur <mdablusur@gmail.com>
2024-06-26 19:34:21 +0600

    ALL ADD

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,18 @@
-import Header from "./components/Header"
+import Banner from "./components/Banner";
+// import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Service from "./components/Service";
+import Subheading from "./components/Subheading";
 
 function App() {
-
-
   return (
     <>
-        <Header/>
+      <Navbar />
+      <Banner />
+      <Service />
+      <Subheading />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
